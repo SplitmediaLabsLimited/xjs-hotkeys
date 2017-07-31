1e31f1a3f565be8a672b27024c1e5b0975926968
NeTe <nete@splitmedialabs.com>
2017-07-31 21:17:20 +0800

    apply location and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import XUIKeyStrokes from "./js/component/XUIKeyStrokes";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<XUIKeyStrokes />, div);
 });
