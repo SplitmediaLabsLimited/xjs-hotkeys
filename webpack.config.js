ee61dc34142ad1f36d4b828239fc32cba0e8048f
NeTe <nete@splitmedialabs.com>
2017-10-10 20:10:15 +0800

    fix library name

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env = {}) => {
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: 'xjs-hotkeys.bundle.js',
-            library: 'XJSHotKeys',
+            library: 'xjs-hotkeys',
             libraryTarget: 'umd',
             umdNamedDefine: true
         },
